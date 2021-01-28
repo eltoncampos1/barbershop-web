ba153d113754085dc8e4ca6e27949b5eef531f03
elnto de campos <eltoncampos36@gmail.com>
2021-01-28 01:20:19 -0300

    [Update] - validate registers with yup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -10,9 +11,22 @@ import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 
 const SignUp: React.FC = () => {
-    function handleSubmit(data:object):void {
-        console.log(data);    
-    }
+    const handleSubmit = useCallback(async(data:object) => {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required(`Nome obrigatório`),
+                email: Yup.string().required(`E-mail obrigatório`).email(`Digite um email válido`),
+                password: Yup.string().min(6, `No minimo 6 dígitos`),
+            })
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+
+        } catch (err) {
+            console.log(err);          
+        }    
+    },[])
 
     return (
         <Container>
