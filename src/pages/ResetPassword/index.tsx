a9c59aadd62f527b64a38ea9bb6edb30036e1574
elton de campos <eltoncampos36@gmail.com>
2021-03-02 03:18:19 -0300

    chore: connect to reset password api and passquery parms

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useRef } from "react";
 import { FiLock } from "react-icons/fi";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import * as Yup from "yup";
 
 import Input from "../../components/Input";
@@ -14,6 +14,7 @@ import getValidationErrors from "../../utils/getValidationErrors";
 
 import { Container, Content, Background, AnimationContainer } from "./styles";
 import { useToast } from "../../hooks/toast";
+import api from "../../services/api";
 
 interface ResetPasswordFormData {
   password: string;
@@ -25,6 +26,7 @@ const ResetPassword: React.FC = () => {
 
   const { addToast } = useToast();
   const history = useHistory();
+  const location = useLocation();
 
   const handleSubmit = useCallback(
     async (data: ResetPasswordFormData) => {
@@ -41,6 +43,19 @@ const ResetPassword: React.FC = () => {
 
         await schema.validate(data, {
           abortEarly: false,
+        });
+
+        const { password, password_confirmation } = data;
+        const token = location.search.replace("?token=", "");
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post("/password/reset", {
+          password,
+          password_confirmation,
+          token,
         });
 
         history.push("/");
@@ -60,7 +75,7 @@ const ResetPassword: React.FC = () => {
         });
       }
     },
-    [addToast, history]
+    [addToast, history, location.search]
   );
 
   return (
