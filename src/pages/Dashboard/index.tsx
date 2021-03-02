8c2996557058d7b70eb265e2694ad7e687b92e43
elton de campos <eltoncampos36@gmail.com>
2021-03-02 03:53:06 -0300

    chore: make datas dinamic

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,7 +6,7 @@ import { FiPower } from "react-icons/fi";
 import { useAuth } from "../../hooks/auth";
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
 
   return (
     <Container>
@@ -15,13 +15,10 @@ const Dashboard: React.FC = () => {
           <img src={logoImg} alt="GOBarber" />
 
           <Profile>
-            <img
-              src="https://avatars.githubusercontent.com/u/56568406?s=460&u=6341df1b4aadb55298fca931b7c88d957896f2c8&v=4"
-              alt="Elton Campos"
-            />
+            <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem-vindo,</span>
-              <strong>Elton Campos</strong>
+              <strong>{user.name}</strong>
             </div>
           </Profile>
 
