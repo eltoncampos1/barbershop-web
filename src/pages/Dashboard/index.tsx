ce69b7ac656a5e9624fcaf77279d26535060dc74
elton de campos <eltoncampos36@gmail.com>
2021-03-02 03:47:43 -0300

    chore: create signout button

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,29 +3,34 @@ import { Container, Header, HeaderContent, Profile } from "./styles";
 
 import logoImg from "../../assets/logo.svg";
 import { FiPower } from "react-icons/fi";
+import { useAuth } from "../../hooks/auth";
 
-const Dashboard: React.FC = () => (
-  <Container>
-    <Header>
-      <HeaderContent>
-        <img src={logoImg} alt="GOBarber" />
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
 
-        <Profile>
-          <img
-            src="https://avatars.githubusercontent.com/u/56568406?s=460&u=6341df1b4aadb55298fca931b7c88d957896f2c8&v=4"
-            alt="Elton Campos"
-          />
-          <div>
-            <span>Bem-vindo,</span>
-            <strong>Elton Campos</strong>
-          </div>
-        </Profile>
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GOBarber" />
 
-        <button type="button">
-          <FiPower />
-        </button>
-      </HeaderContent>
-    </Header>
-  </Container>
-);
+          <Profile>
+            <img
+              src="https://avatars.githubusercontent.com/u/56568406?s=460&u=6341df1b4aadb55298fca931b7c88d957896f2c8&v=4"
+              alt="Elton Campos"
+            />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>Elton Campos</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 export default Dashboard;
