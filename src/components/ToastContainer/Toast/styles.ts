97319ae9cf1e11efec80b606fe8ab44418a1c9f2
elton de campos <eltoncampos36@gmail.com>
2021-03-02 02:45:47 -0300

    refactor: transform hasDrescription props in number

--- src/components/ToastContainer/Toast/styles.ts
+++ src/components/ToastContainer/Toast/styles.ts
@@ -4,7 +4,7 @@ import styled, { css } from 'styled-components'
 
 interface ContainerProps {
     type?: "success" | "error" | "info";
-    hasDescription: boolean;
+    hasDescription: number;
 }
 
 const toastTypeVariations = {
