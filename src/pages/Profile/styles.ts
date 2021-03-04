4ff888e387a72e026d1458a0ca650048afb58e12
elton de campos <eltoncampos36@gmail.com>
2021-03-04 01:30:39 -0300

    chore: add avatar input css styles

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,7 +1,6 @@
 import { shade } from 'polished';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
-import SignUp from '../../assets/sign-up.png'
 
 
 export const Container = styled.div`
@@ -16,41 +15,21 @@ export const Content = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin: 0 auto;
 
   width: 100%;
-  max-width: 700px;
-
-  
-`;
-
-const appearFromRight = keyframes`
-  from {
-    opacity: 0;
-    transform: translateX(50px);
-  }
-
-  to {
-    opacity: 1;
-    transform: translateX(0);
-  }
-`;
-
-
-
-export const AnimationContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  animation: ${appearFromRight} 1s;
 
   form {
       margin: 80px 0;
       width: 340px;
       text-align: center;
+      display: flex;
+      flex-direction: column;
 
       h1 {
         margin-bottom: 24px;
+        font-size: 20px;
+        text-align: left;
       }
 
 
@@ -64,33 +43,53 @@ export const AnimationContainer = styled.div`
             &:hover {
                 color: ${shade(0.2, '#f4ede8')}
             }
-        }  
+        } 
+
+        input[name='old_password']  {
+          margin-top: 24px;
+        }
 
     }
-
-     > a {
-        color: #ff9000;
-        display: block;
-        margin-top: 24px;
-        text-decoration: none;
-        transition: color 0.2s;
-
-        display: flex;
-        align-items: center;
-
-        svg {
-            margin-right: 16px;
-        }
-
-        &:hover {
-            color: ${shade(0.2, '#ff9000')}
-        }
-     } 
+  
 `;
 
-export const Background = styled.div`
-  flex: 1;
-  background: url(${SignUp}) no-repeat center;
-  background-size: cover;
+export const AvatarInput = styled.div`
+  margin-bottom: 32px;
+  position: relative;
+  align-self: center;
+
+    img {
+      width: 186px;
+      height: 186px;
+      border-radius: 50%;
+    }
+
+    button {
+      position: absolute;
+      width: 48px;
+      height: 48px;
+      background: #ff9000;
+      border-radius: 50%;
+      right: 0;
+      bottom: 0;
+      border: 0;
+      transition: background-color 0.2s;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      svg {
+        width: 20px;
+        height: 20px;
+        color: #312e38;
+      }
+
+      &:hover {
+        background: ${shade(0.2, '#ff9000')}
+      }
+    }
 `;
+
+
 
